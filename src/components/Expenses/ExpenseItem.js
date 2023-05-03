94bdeaf648c3c9191568751b74eea140e7f5dca1
yanira2 <ruth.cortez@puzzle.tech>
2023-05-03 16:44:25 -0600

    Seccion 4 Finalizada

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,11 @@
-import './ExpenseDate';
-import Card from '../UI/Card';
-import './Expenseltem.css';
-
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
-
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 const ExpenseItem = (props) => {
-     return (
+  return (
     <Card className='expense-item'>
-       <ExpenseDate date={props.date }/>
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
@@ -16,5 +13,4 @@ const ExpenseItem = (props) => {
     </Card>
   );
 }
-
 export default ExpenseItem;
